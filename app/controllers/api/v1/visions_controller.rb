d942509fc9d101526d422b646e2bc7a7256428cf
gracenak <gracenak@gmail.com>
2021-02-15 21:08:55 -0700

    add create action in controller

--- app/controllers/api/v1/visions_controller.rb
+++ app/controllers/api/v1/visions_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::VisionsController < ApplicationController
     def index
-
+        visions = Vision.all
+        render json: visions
     end
 
     def create
-        
+        vision = Vision.new(vision_params)
+        if vision.save
+            render json: vision, status: :accepted
+        else
+            render json: {errors: vision.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def vision_params
+        params.require(:vision).permit(:title, :description, :image_url, :user_id)
     end
 end
