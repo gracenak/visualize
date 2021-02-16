89c8c3e2b1635312917fb560afe87b86576df380
gracenak <gracenak@gmail.com>
2021-02-15 21:55:18 -0700

    add fast json_api gem and add serializer to the controller

--- app/controllers/api/v1/visions_controller.rb
+++ app/controllers/api/v1/visions_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::VisionsController < ApplicationController
     def index
         visions = Vision.all
-        render json: visions
+        render json: VisionSerializer.new(visions)
     end
 
     def create
         vision = Vision.new(vision_params)
         if vision.save
-            render json: vision, status: :accepted
+            render json: VisionSerializer.new(vision), status: :accepted
         else
             render json: {errors: vision.errors.full_messages}, status: :unprocessible_entity
         end
